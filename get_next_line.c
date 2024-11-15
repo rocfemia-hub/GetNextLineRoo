0619854702e9315518c241fbc4c0d61c5491c508
Your Name <you@example.com>
2024-11-15 16:36:05 +0100

    comenzando funcion ft_find_n

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,38 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:27:09 by roo               #+#    #+#             */
-/*   Updated: 2024/11/06 17:25:40 by roo              ###   ########.fr       */
+/*   Updated: 2024/11/15 16:19:50 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *ft_find_n(int fd, char *buffer)
+{
+    int i;
+    char *str;
+
+    i = 0;
+    while (buffer[i] != '\n')
+    {
+        str[i] = buffer[i];
+        i++;
+    }
+    if (buffer[i] == '\n')
+        str[i] == '\n';
+    else if
+        return (NULL);
+    return (str);
+}
 char *get_next_line(int fd)
 {
     static char *buffer;
     char *line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+    {
+        return (NULL);
+    }
 
     return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:26:57 by roo               #+#    #+#             */
-/*   Updated: 2024/11/06 11:38:29 by roo              ###   ########.fr       */
+/*   Updated: 2024/11/14 18:15:08 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
 
 char *get_next_line(int fd);
 
